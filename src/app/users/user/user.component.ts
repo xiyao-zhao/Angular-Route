c2ff68002ef43c6cbad32727bc33747e2b9d30a8
Xiyao Zhao <xiyao.zhao28@gmail.com>
2022-02-24 17:45:42 -0500

    Initial commit

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+      // when there's no change within the component
+    this.user = {
+        id: this.route.snapshot.params['id'],
+        name: this.route.snapshot.params['name']
+    };
+    // params is observables, get changes when parameter changes
+    this.route.params
+        .subscribe(
+            (params: Params) => {
+                this.user.id = params['id'];
+                this.user.name = params['name'];
+            }
+        );
+  }
+
+}
